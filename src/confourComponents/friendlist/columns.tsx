be1f72888be40668a7d6ff4d65feaf974f82c613
Penderekki <penderekki@gmail.com>
2024-02-15 16:07:10 +0100

    added formatting to one header in columns

--- src/confourComponents/friendlist/columns.tsx
+++ src/confourComponents/friendlist/columns.tsx
@@ -1,16 +1,30 @@
+import React from "react";
 import { ColumnDef } from "@tanstack/react-table";
 
 export type Friend = {
   id: string;
   username: string;
-  status: "online" | "offline" | "busy" | "do-not-disturb";
+  status: "online" | "offline" | "busy" | "dnd";
   email: string;
 };
 
-export const columns: ColumnDef<Friend>[] = [
+const statusMap: { [K in Friend["status"]]: string } = {
+  online: "Online",
+  offline: "Offline",
+  busy: "Busy",
+  dnd: "Do Not Disturb",
+};
+
+export const columns: ColumnDef<Friend, keyof Friend>[] = [
   {
     accessorKey: "status",
-    header: "Status",
+    header: () => <div className="text-right">Status</div>,
+    cell: ({ getValue }) => {
+      const status = getValue() as Friend["status"];
+      const label = statusMap[status];
+
+      return <div className="text-right font-medium">{label}</div>;
+    },
   },
   {
     accessorKey: "email",
