daa018f8ce675ed590dc627b1a1e13cfc9f4d2d4
Penderekki <penderekki@gmail.com>
2024-02-16 12:35:51 +0100

    made email hovercard instead

--- src/confourComponents/friendlist/columns.tsx
+++ src/confourComponents/friendlist/columns.tsx
@@ -10,6 +10,11 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
 
 export type Friend = {
   id: string;
@@ -22,16 +27,14 @@ const statusMap: { [K in Friend["status"]]: string } = {
   online: (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      fill="none"
       viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="#10B981" // Tailwind CSS Green-500
+      fill="#10B981" // Tailwind CSS Green-500
       className="w-6 h-6"
     >
       <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+        fillRule="evenodd"
+        d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+        clipRule="evenodd"
       />
     </svg>
   ),
@@ -96,22 +99,35 @@ export const columns: ColumnDef<Friend, keyof Friend>[] = [
       return <div className="text-right font-medium">{labelOrIcon}</div>;
     },
   },
-  {
-    accessorKey: "email",
-    header: () => <div className="text-right">Email</div>,
-    cell: ({ getValue }) => {
-      const email = getValue() as Friend["email"];
-
-      return <div className="text-right font-medium">{email}</div>;
-    },
-  },
+  // {
+  //   accessorKey: "email",
+  //   header: () => <div className="text-right">Email</div>,
+  //   cell: ({ getValue }) => {
+  //     const email = getValue() as Friend["email"];
+  //
+  //     return <div className="text-right font-medium">{email}</div>;
+  //   },
+  // },
   {
     accessorKey: "username",
     header: () => <div className="text-right">Username</div>,
-    cell: ({ getValue }) => {
-      const username = getValue() as Friend["username"];
-
-      return <div className="text-right font-medium">{username}</div>;
+    cell: ({ row }) => {
+      const username = row.original.username;
+      const email = row.original.email;
+      return (
+        <div className="text-right font-medium">
+          <HoverCard>
+            <HoverCardTrigger asChild>
+              <Button variant="link">{username}</Button>
+            </HoverCardTrigger>
+            <HoverCardContent className="w-fit p-4 shadow-lg rounded-lg overflow-hidden">
+              <div className="flex mx-auto whitespace-normal break-words">
+                {email}
+              </div>
+            </HoverCardContent>
+          </HoverCard>
+        </div>
+      );
     },
   },
   {
